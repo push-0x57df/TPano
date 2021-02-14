a3e2f56d98272e480d7d5aceb62562ed5f64640a
黄子楠 <1323592980@qq.com>
2021-02-14 23:42:36 +0800

    修复bug：查看器大小改变后点击热点没反应

--- dist/tpano.js
+++ dist/tpano.js
@@ -114,8 +114,8 @@ function TPano(d) {
         const mouse = new THREE.Vector2();
         function onMouseMove(event) {
             // 将鼠标位置归一化为设备坐标。x 和 y 方向的取值范围是 (-1 to +1)
-            mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-            mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+            mouse.x = (event.clientX / el.clientWidth) * 2 - 1;
+            mouse.y = - (event.clientY / el.clientHeight) * 2 + 1;
             render();
         }
 
